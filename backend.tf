12c16d520013d98c791879b8b1ab7ef9ddc3fd47
victoria-cloud-AE <victoriaenuma@gmail.com>
2026-01-10 09:16:32 +0100

    CICD pipeline added

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "enumavictoriaS3-2026TS"
+    bucket = "enumavictorias3-2026ts"
     key    = "enumavictoria/prodution/terraform.tfstate"
     region = "us-east-1"
   }
