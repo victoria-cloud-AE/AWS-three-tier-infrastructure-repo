1c107a7d7cb3a7641d0cf7e6096868cced8f3037
victoria-cloud-AE <victoriaenuma@gmail.com>
2026-01-11 11:35:47 +0100

    infrastructure deployment

--- module-eks/output.tf
+++ module-eks/output.tf
@@ -1,13 +1,13 @@
-output "nginx_ingress_lb_dns" {
-  value = data.aws_lb.nginx_ingress.dns_name
-}
+# output "nginx_ingress_lb_dns" {
+#   value = data.aws_lb.nginx_ingress.dns_name
+# }
 
-output "nginx_lb_ip" {
-  value = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
-}
-output "nginx_ingress_load_balancer_hostname" {
-  value = data.aws_lb.nginx_ingress.dns_name
-}
+# output "nginx_lb_ip" {
+#   value = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
+# }
+# output "nginx_ingress_load_balancer_hostname" {
+#   value = data.aws_lb.nginx_ingress.dns_name
+# }
 # Outputs for the EKS cluster
 # output "cluster_name" {
 #   value       = aws_eks_cluster.eks.name
